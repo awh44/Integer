2c9ce418d0dfd1b85c519f28f41adc6f57bbb526
Austin Herring <awh44@drexel.edu>
2014-11-09 21:40:20 -0500

    Added inverse FFT; cleaned up.
    Added function for computing inverse FFT to FFT.c (again based on Wilf's description, by taking the conjugates at roots of unity, applying regular FFT, and taking the conjugates again) and did some cleanup in that file. I think there's currently a sign problem in the FFT function.

--- FFT.c
+++ FFT.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
+#include <string.h>
 
-const long double two_pi = 2 * M_PI;
+const long double TWO_PI = 2 * M_PI;
 
 typedef struct
 {
@@ -12,42 +13,72 @@ typedef struct
 
 Complex complex_add(Complex a, Complex b)
 {
-	Complex result;
-	result.real = a.real + b.real;
-	result.imag = a.imag + b.imag;
+	Complex result = { a.real + b.real, a.imag + b.imag };
 	return result;
 }
 
 Complex complex_multiply(Complex a, Complex b)
 {
-	Complex result;
-	result.real = a.real * b.real - a.imag * b.imag;
-	result.imag = a.real * b.imag + a.imag * b.real;
+	Complex result = { a.real * b.real - a.imag * b.imag, a.real * b.imag + a.imag * b.real };
 	return result;
 }
 
-Complex *FFT(int n, int *x, int start, int step_size)
+Complex complex_conjugate_over_n(Complex a, int n)
+{
+	Complex result = { a.real / n, -a.imag / n };
+	return result;
+}
+
+Complex complex_conjugate(Complex a)
+{
+	Complex result = { a.real, -a.imag };
+	return result;
+}
+
+Complex *compute_conjugates(int n, Complex *x)
+{
+	Complex *result = malloc(n * sizeof *result);
+	int i;
+	for (i = 0; i < n; i++)
+	{
+		result[i] = complex_conjugate(x[i]);
+	}
+
+	return result;
+}
+
+Complex *compute_conjugates_over_n(int n, Complex *x)
+{
+	Complex *result = malloc(n * sizeof *result);
+	int i;
+	for (i = 0; i < n; i++)
+	{
+		result[i] = complex_conjugate_over_n(x[i], n);
+	}
+
+	return result;
+}
+
+Complex *FFT_worker(int n, Complex *x, int start, int step_size)
 {
 	Complex *result = malloc(n * sizeof *result);
 	if (n == 1)
 	{
-		result[0].real = x[start];
+		result[0] = x[start];
 		return;
 	}
 
 	int n_over_2 = n / 2;
 	int ss_times_2 = step_size * 2;
-	Complex *u = FFT(n_over_2, x, start, ss_times_2),
-			*v = FFT(n_over_2, x, start + step_size, ss_times_2);
+	Complex *u = FFT_worker(n_over_2, x, start, ss_times_2);
+	Complex *v = FFT_worker(n_over_2, x, start + step_size, ss_times_2);
 
-	long double mult_val = two_pi / n;
+	long double mult_val = TWO_PI / n;
 	int j;
 	for (j = 0; j < n; j++)
 	{
-		Complex tau;
 		long double mult_val_j = mult_val * j;
-		tau.real = cosl(mult_val_j);
-		tau.imag = sinl(mult_val_j);
+		Complex tau = { cosl(mult_val_j), sinl(mult_val_j) };
 		int j_mod_n_over_2 = j % n_over_2;
 		result[j] = complex_add(u[j_mod_n_over_2], complex_multiply(tau, v[j_mod_n_over_2]));
 	}
@@ -57,17 +88,57 @@ Complex *FFT(int n, int *x, int start, int step_size)
 	return result;
 }
 
-int main()
+Complex *FFT(int n, Complex *x)
 {
-	int inp[] = {0, 6, -5, 1};
-	Complex *result = FFT(4, inp, 0, 1);
+	return FFT_worker(n, x, 0, 1);
+}
 
+Complex *inverse_FFT(int n, Complex *x)
+{
+	Complex *conjugates = compute_conjugates(n, x);
+	return compute_conjugates_over_n(n, FFT_worker(n, conjugates, 0, 1));
+}
+
+void print_complex_number(Complex x)
+{
+	printf("%Lf + %Lfi\n", x.real, x.imag);
+}
+
+void print_complex_numbers(int n, Complex *x)
+{
 	int i;
-	for (i = 0; i < 4; i++)
+	for (i = 0; i < n; i++)
 	{
-		printf("%Lf + %Lfi\n", result[i].real, result[i].imag);
+		print_complex_number(x[i]);
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc < 2)
+	{
+		printf("Please enter at least one number of which to compute the FFT.\n");
+		return 1;
+	}
+	
+	int num_things = argc - 1;
+	Complex *inp = malloc(num_things * sizeof *inp);
+	int i;
+	for (i = 0; i < num_things; i++)
+	{
+		inp[i].real = atoi(argv[i + 1]);
+		inp[i].imag = 0;
 	}
 
-	free(result);
+	Complex *resultFFT = FFT(num_things, inp);
+	print_complex_numbers(num_things, resultFFT);
+
+	printf("\n\n");
+
+	Complex *resultInverseFFT = inverse_FFT(num_things, resultFFT);
+	print_complex_numbers(num_things, resultInverseFFT);
+
+	free(resultFFT);
+	free(resultInverseFFT);
 	return 0;
 }
